76a1b7c03e6a585efdcdeb867b5eadec56b6a12b
fazeem11 <fazeemzayn4@gmail.com>
2024-08-16 05:54:59 -0700

    Add files via upload

---
+++ 7loops.py
@@ -0,0 +1,75 @@
+#while loop
+# i=1
+# while i<17:
+#     print(i)
+#     i +=3
+
+# i=2
+# while i<9:
+#     print(i)
+#     i+=2
+
+# contro statement(break and continue)
+
+# break
+
+# i=1
+# a=int(input("enter a number"))
+# while i<a:
+#     print(i) 
+#     if(i==8):
+#         break
+#     i+=1
+
+#continue
+
+# i=0
+# while i<6:
+#     i+=1
+#     if i==3:
+#         continue
+#     print(i)
+
+# i=int(input("enter a number for starting")) #starting number
+# a=int(input("enter a number for ending")) #end
+# b=int(input("enter a number which want to be continue")) #break/continue
+# while i<a:
+#     i+=1
+#     if i==b:
+#         continue
+#     print(i)
+
+#for loop
+
+# fruits =["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+#     if x=="banana":
+#         break
+
+x=input("enter a fruit:")
+a=input("enter a fruit:")
+b=input("enter a fruit:")
+e=input("enter a fruit:")
+c=[x,a,b,e]
+
+d=input("enter fruit need to be continue/gone:")
+for i in c:
+    print(i)
+    if d==i:
+        break
+        
+
+
+   
+
+
+    
+    
+    
+
+    
+
+
+
+
