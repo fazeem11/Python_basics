6a2835b4d927acda5cc8c1c6ff90fade7d1cd48a
fazeem11 <fazeemzayn4@gmail.com>
2024-08-20 00:03:16 -0700

    Add files via upload

---
+++ fazeem/12 lambda.py
@@ -0,0 +1,64 @@
+# LAMBDA
+
+# greet=lambda:print("hello world")
+# greet()
+# greet_user=lambda name: print ('hey there', name)
+# greet_user('delhi')
+# x=lambda name: print ("my name is",name)
+# x("fazeem")
+
+# MAP(function(map),iterable(list))
+
+# list1 = {1,2,3,4,5,6}
+# sqr=list(map(lambda x:x**2,list1))
+# print(sqr)
+
+# Filter
+
+# even=list(filter(lambda x:x%2==0,list1))
+# for i in even:
+#     print(i)
+
+# odd=list(filter(lambda x:x%2!=0,list1))
+# print(odd)
+
+# REDUCE
+
+# list1=[1,2,3,4,5,6]
+# from functools import reduce 
+# prdct=reduce(lambda x,y:x*y,list1)
+# print(prdct)
+
+# USER DEFINED
+
+# def my_function(fname):
+#     print( fname +"any")
+# my_function("emil")
+# my_function("tobi")
+# my_function("linu")
+
+# def x(y):
+#     print("th"+y)
+# x("hari")
+# x("joju")
+# x("praji")
+
+# def add1(x,y):
+#     g=x+y
+#     print("sum is:",g)
+# add1(2,3)
+# add1(9,10)
+
+# def add1(x=7,y=9):
+#     g=x+y
+#     print("sum is:",g)
+# add1(2,3)
+# add1(x=3)
+# add1()
+
+
+
+
+
+
+
---
+++ fazeem/13 Arguments.py
@@ -0,0 +1,16 @@
+# KEYWORD ARGUMENT
+
+# def display_info(f_name,l_name):
+#     print("f_name:",f_name)
+#     print("l_name:",l_name)
+# display_info(l_name="eric",f_name="cart")
+
+# ARBITARY ARGUMENT
+
+# def find_sum(*numbers):
+#     result=0
+#     for num in numbers:
+#         result = result+num
+#     print("sum=",result)
+# find_sum(1,2,3)
+# find_sum(78,9)
